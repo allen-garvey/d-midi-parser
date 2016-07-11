e35a1c648ed992cbb1d0ac9a76b43d7374b25d4e
Allen Garvey <allen@allengarvey.com>
2016-07-10 21:09:30 -0400

    edit visibility of midifile bit manipulation functions so that midi writer functions can use them in midiwriter file

--- midi/midifile.d
+++ midi/midifile.d
@@ -301,7 +301,6 @@ struct MIDIEvent{
     }
 }
 
-private:
 
 void writeVarLen(ref ubyte[] buffer, uint value){
     uint buf;
@@ -377,24 +376,4 @@ else version(BigEndian){
 }
 else{
     static assert("Unknown endian!");
-}
-
-
-//Reference for MIDI spec:
-//http://ccarh.org/courses/253/handout/smf/
-MThd_Chunk createMidiHeaderChunk(ushort numTracks, ushort ticksPerBeat=256){
-    MThd_Chunk midiHeader;
-    //id and length must always be set to the following values
-    midiHeader.length = 6;
-    //midi format 1 is multi-track, 0 is single track
-    midiHeader.format = 1;
-    midiHeader.numTracks = numTracks;
-    midiHeader.ticksPerBeat = ticksPerBeat;
-
-    //numeric values must be converted to big endian
-    hostToBigEndian(&midiHeader);
-
-    midiHeader.id = ['M', 'T', 'h', 'd'];
-
-    return midiHeader;
 }
--- midi/midiwriter.d
+++ midi/midiwriter.d
@@ -9,3 +9,21 @@ void writeMidiFile(string filename, MIDIEvent[][] tracks){
 }
 
 
+//Reference for MIDI spec:
+//http://ccarh.org/courses/253/handout/smf/
+MThd_Chunk createMidiHeaderChunk(ushort numTracks, ushort ticksPerBeat=256){
+    MThd_Chunk midiHeader;
+    //id and length must always be set to the following values
+    midiHeader.length = 6;
+    //midi format 1 is multi-track, 0 is single track
+    midiHeader.format = 1;
+    midiHeader.numTracks = numTracks;
+    midiHeader.ticksPerBeat = ticksPerBeat;
+
+    //numeric values must be converted to big endian
+    hostToBigEndian(&midiHeader);
+
+    midiHeader.id = ['M', 'T', 'h', 'd'];
+
+    return midiHeader;
+}
