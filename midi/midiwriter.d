7679d8dd576e22c212ee1c6a0fabb8fce4536426
Allen Garvey <allengarvey@hotmail.com>
2016-07-12 22:53:22 -0400

    add contracts for return results of create midi header and track chunk functions to make sure they return chunks with valid ids

--- midi/midiwriter.d
+++ midi/midiwriter.d
@@ -13,31 +13,39 @@ void writeMidiFile(string filename, MIDIEvent[][] tracks){
 
 //Reference for MIDI spec:
 //http://ccarh.org/courses/253/handout/smf/
-MThd_Chunk createMidiHeaderChunk(ushort numTracks, ushort ticksPerBeat=256){
-    MThd_Chunk midiHeader;
-    //id and length must always be set to the following values
-    midiHeader.length = 6;
-    //midi format 1 is multi-track, 0 is single track
-    midiHeader.format = 1;
-    midiHeader.numTracks = numTracks;
-    midiHeader.ticksPerBeat = ticksPerBeat;
+MThd_Chunk createMidiHeaderChunk(ushort numTracks, ushort ticksPerBeat=256)
+    out(header){
+        assert(header.id[] == "MThd", "Incorrect midi header chunk id");
+    }
+    body{
+        MThd_Chunk midiHeader;
+        //id and length must always be set to the following values
+        midiHeader.length = 6;
+        //midi format 1 is multi-track, 0 is single track
+        midiHeader.format = 1;
+        midiHeader.numTracks = numTracks;
+        midiHeader.ticksPerBeat = ticksPerBeat;
 
-    //numeric values must be converted to big endian
-    hostToBigEndian(&midiHeader);
+        //numeric values must be converted to big endian
+        hostToBigEndian(&midiHeader);
 
-    midiHeader.id = ['M', 'T', 'h', 'd'];
+        midiHeader.id = ['M', 'T', 'h', 'd'];
 
-    return midiHeader;
-}
+        return midiHeader;
+    }
 
-MTrk_Chunk createMidiTrackChunk(uint length){
-    MTrk_Chunk trackChunk;
-    trackChunk.length = length;
-    hostToBigEndian(&trackChunk);
+MTrk_Chunk createMidiTrackChunk(uint length)
+    out(chunk){
+        assert(chunk.id[] == "MTrk", "Incorrect midi track chunk id");
+    }
+    body{
+        MTrk_Chunk trackChunk;
+        trackChunk.length = length;
+        hostToBigEndian(&trackChunk);
 
-    trackChunk.id = ['M', 'T', 'r', 'k'];
-    return trackChunk;
-}
+        trackChunk.id = ['M', 'T', 'r', 'k'];
+        return trackChunk;
+    }
 
 
 
